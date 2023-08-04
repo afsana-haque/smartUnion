ca8b18f50aee849e04db753ef1936aa9d899bcc7
Mosiur411 <mosiurislamwebdesign@gmail.com>
2023-08-04 21:32:48 +0600

    folder st create

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # Smartunion_landing Package User
-#### react 18.2.0
-#### aos 2.3.4
-#### react-hook-form 7.45.2,
-#### yup 
-#### 
-#### 
-#### 
-#### 
-
---
+++ documentation/package.js
@@ -0,0 +1,15 @@
+/*
+1. "react 18.2.0"
+2. "aos 2.3.4"
+3. "react-hook-form 7.45.2"
+4. "yup"
+5. "react-firebase-hooks"
+6. "react-firebase-hooks"
+7. "firebase"
+8. "react-redux"
+9. "reduxjs/toolkit"
+10. "react-jwt"
+11. "react-icons/all-files" 
+12. "react-icons --save" 
+13. "react-helmet" 
+*/
